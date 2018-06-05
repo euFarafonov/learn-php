752d5940fd688f857e7ec7b0da03b833a50bdb92
udjin1982 <frxvip@gmail.com>
2018-06-06 00:41:54 +0300

    fixed template

--- task2/templates/index.php
+++ task2/templates/index.php
@@ -49,15 +49,5 @@
 <div>Memory after clear = <?=$memClear2?></div>
 <div>Memory = <?=$memRead32?></div>
 
-<?php
-if (isset($_SESSION['msg']))
-{
-    echo $_SESSION['msg'];
-    unset($_SESSION['msg']);
-}
-?>
-
-
-
 </body>
 </html>
