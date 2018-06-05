06d10055f2b428eb70bbd8faf41ae67627658a2f
udjin1982 <frxvip@gmail.com>
2018-06-06 00:39:07 +0300

    fixed task 2

--- task2/index.php
+++ task2/index.php
@@ -6,27 +6,59 @@ $calc = new Calc();
 
 $calc->setVarA(5);
 $calc->setVarB(10);
-$res1 = $calc->result();
 
-$echo 'Num A = ' . $calc->A;
-$echo 'Num B = ' . $calc->B;
+$a = $calc->getVarA();
+$b = $calc->getVarB();
 
-$echo 'Mem = ' . $calc->Mem;
-$echo 'A + B = ' . $calc->sum();
-$echo 'A - B = ' . $calc->ded();
-$echo 'A * B = ' . $calc->mult();
-$echo 'A / B = ' . $calc->div();
-$echo 'sqrtA = ' . $calc->sqrt($calc->A);
-$echo 'sqrtB = ' . $calc->sqrt($calc->B);
-$echo '1 / A = ' . $calc->divX($calc->A);
-$echo '1 / B = ' . $calc->divX($calc->B);
-$echo 'A percent B = ' . $calc->perc($calc->A, $calc->B);
-$echo 'mem + A = ' . $calc->memAdd($calc->A);
-$echo 'mem + B = ' . $calc->memAdd($calc->B);
-$echo 'mem - A = ' . $calc->memDiv($calc->A);
-$echo 'mem Clear = ' . $calc->memClear();
-$echo 'Mem = ' . $calc->Mem();
+$sum = $calc->sum();
+$ded = $calc->ded();
+$mult = $calc->mult();
+$div = $calc->div();
 
+$sqrtA = $calc->sqrtVar($calc->getVarA());
+$sqrtB = $calc->sqrtVar($calc->getVarB());
+$divXA = $calc->divX($calc->getVarA());
+$divXB = $calc->divX($calc->getVarB());
+
+$percentAB = $calc->percent($calc->getVarA(), $calc->getVarB());
+$percentBA = $calc->percent($calc->getVarB(), $calc->getVarA());
+
+$memRead1 = $calc->memRead();
+$memAddA = $calc->memAdd($calc->getVarA());
+$memAddB = $calc->memAdd($calc->getVarB());
+$memDivA = $calc->memDiv($calc->getVarA());
+$memRead2 = $calc->memRead();
+$memClear = $calc->memClear();
+$memRead3 = $calc->memRead();
+
+$calc2 = new Calc();
+
+$calc2->setVarA(27);
+$calc2->setVarB(0);
+
+$a2 = $calc2->getVarA();
+$b2 = $calc2->getVarB();
+
+$sum2 = $calc2->sum();
+$ded2 = $calc2->ded();
+$mult2 = $calc2->mult();
+$div2 = $calc2->div();
+
+$sqrtA2 = $calc2->sqrtVar($calc2->getVarA());
+$sqrtB2 = $calc2->sqrtVar($calc2->getVarB());
+$divXA2 = $calc2->divX($calc2->getVarA());
+$divXB2 = $calc2->divX($calc2->getVarB());
+
+$percentAB2 = $calc2->percent($calc2->getVarA(), $calc2->getVarB());
+$percentBA2 = $calc2->percent($calc2->getVarB(), $calc2->getVarA());
+
+$memRead12 = $calc2->memRead();
+$memAddA2 = $calc2->memAdd($calc2->getVarA());
+$memAddB2 = $calc2->memAdd($calc2->getVarB());
+$memDivA2 = $calc2->memDiv($calc2->getVarA());
+$memRead22 = $calc2->memRead();
+$memClear2 = $calc2->memClear();
+$memRead32 = $calc2->memRead();
 
 require_once TEMPLATES.TEMPLATE;
 ?>
--- task2/libs/Calc.php
+++ task2/libs/Calc.php
@@ -4,73 +4,80 @@ class Calc
     private $A;
     private $B;
     private $Mem = 0;
-    private $result = Array();
 
     public function setVarA($var)
     {
-	$this->A = (int)$var;
+	   $this->A = (int)$var;
     }
     
     public function setVarB($var)
     {
         $this->B = (int)$var;
     }
-
-    private function sum()
+    
+    public function getVarA()
     {
-	return ($this->A + $this->B);
+        return $this->A;
+    }
+    
+    public function getVarB()
+    {
+        return $this->B;
+    }
+    
+    public function sum()
+    {
+	   return ($this->A + $this->B);
     }
 
-    private function ded()
+    public function ded()
     {
-	return ($this->A - $this->B);
+	   return ($this->A - $this->B);
     }
 
-    private function mult()
+    public function mult()
     {
-	return ($this->A * $this->B);
+	   return ($this->A * $this->B);
     }
 
-    private function div()
+    public function div()
     {
-	return ($this->B === 0) ? DIV_ERROR : ($this->A / $this->B);
+	   return (0 === $this->B) ? DIV_ERROR : ($this->A / $this->B);
     }
 
-    private function sqrtVar($var)
+    public function sqrtVar($var)
     {
-	return sqrt($var);	
+	   return sqrt($var);	
     }
 
-    private function divX($var)
+    public function divX($var)
     {
-	return ($var === 0) ? DIV_ERROR : (1 / $var);
+	   return (0 === $var) ? DIV_ERROR : (1 / $var);
     }
 
-    private function perc($var, $per)
+    public function percent($x, $y)
     {
-	return ($this->A / 100 * $this->B);
+	   return ($x / 100 * $y);
     }
 
-    private function memRead()
+    public function memRead()
     {
-	return $this->Mem;
+	   return $this->Mem;
     }
 
-    private function memAdd($var)
+    public function memAdd($var)
     {
-	return $this->Mem += $var;
+	   return $this->Mem += $var;
     }
 
-    private function memDiv($var)
+    public function memDiv($var)
     {
-	return $this->Mem -= $var;
+	   return $this->Mem -= $var;
     }
 
-    private function memClear()
+    public function memClear()
     {
-	return ($this->Mem = 0);
+	   return ($this->Mem = 0);
     }
-
 }
-
 ?>
